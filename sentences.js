94b4154ecf8be20b1dc0945621e3d593831f1fda
umohamed <umohamed@bowdoin.edu>
2023-10-01 12:57:53 -0400

    added the sentences js

---
+++ sentences.js
@@ -0,0 +1,362 @@
+var sentences = [
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "I went to Zimziland because I heard you can get a foobly apple there.",
+    "novel1": "foobly",
+    "novel2": "Zimziland",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "My sister doesn't like to have a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "Everybody I met in Zimziland would love to have a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "Zimziland",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "As soon as I arrived, I noticed a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "I would really love to have a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "One lady offered me a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "Maybe I would prefer a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "I went to a place where it's easy to find a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "The people I met there told me they saw a foobly apple that day.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly apple",
+    "Part": "training",
+    "sentence": "In the end, I decided I'd go looking for a foobly apple.",
+    "novel1": "foobly",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "I didn't find a foobly mipp.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "My sister told me you can get a foobly mipp there.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "My friend Sally said she saw a foobly mipp once.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "There was a foobly mipp just a few steps from here.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "It's absolutely amazing to get to see a foobly mipp in person.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "I thought maybe I'd see a foobly mipp on the next day.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "I also love to have a foobly mipp.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "I imagined that I would find a foobly mipp in Zimziland.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": "Zimziland"
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "On the second day, I saw a foobly mipp.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 1,
+    "Pair": "foobly mipp",
+    "Part": "training",
+    "sentence": "Sometimes I wish it were easier to get a foobly mipp.",
+    "novel1": "foobly",
+    "novel2": "mipp",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "I would love to see a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "I only found a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "She offered me a dodish horse, too.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "I am not sure if I would like a dodish horse better.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "You never know with a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "I hadn't seen a dodish horse for hours.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "There is no doubt, a dodish horse is the best!",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "There are people who prefer a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "On the eleventh day of my trip, I found a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish horse",
+    "Part": "training",
+    "sentence": "I have a picture of a dodish horse.",
+    "novel1": "dodish",
+    "novel2": "",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I would like to have a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I stayed there for a couple of days, hoping I would find a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "At the end of day one, I was tired and just hoped to see a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I went out early to look for a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I wished that they at least would have a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "They also said that they would really like to see a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "Everybody would like to have a dodish geck.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I'm a dodish geck fan!",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "In the end, I brought a dodish geck home.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  },
+  {
+    "Triad": 2,
+    "Pair": "dodish geck",
+    "Part": "training",
+    "sentence": "I took a lot of photos of a dodish geck I saw.",
+    "novel1": "dodish",
+    "novel2": "geck",
+    "novel3": ""
+  }
+]
