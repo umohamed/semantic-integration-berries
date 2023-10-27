56018ade17c670e1ec88b9dcd388692347020802
kavyald <kdoraswa@bowdoin.edu>
2023-10-27 15:08:20 -0400

    added break in priming section, fixed instructions for training

---
+++ experiment/countdown.js
@@ -0,0 +1,95 @@
+var countdown_numbers = [
+	{
+		number: "30"
+	},
+	{
+		number: "29"
+	},
+	{
+		number: "28"
+	},
+	{
+		number: "27"
+	},
+	{
+		number: "26"
+	},
+	{
+		number: "25"
+	},
+	{
+		number: "24"
+	},
+	{
+		number: "23"
+	},
+	{
+		number: "22"
+	},
+	{
+		number: "21"
+	},
+	{
+		number: "20"
+	},
+	{
+		number: "19"
+	},
+	{
+		number: "18"
+	},
+	{
+		number: "17"
+	},
+	{
+		number: "16"
+	},
+	{
+		number: "15"
+	},
+	{
+		number: "14"
+	},
+	{
+		number: "13"
+	},
+	{
+		number: "12"
+	},
+	{
+		number: "22"
+	},
+	{
+		number: "10"
+	},
+	{
+		number: "9"
+	},
+	{
+		number: "8"
+	},
+	{
+		number: "7"
+	},
+	{
+		number: "6"
+	},
+	{
+		number: "5"
+	},
+	{
+		number: "4"
+	},
+	{
+		number: "3"
+	},
+	{
+		number: "2"
+	},
+	{
+		number: "1"
+	},
+	{
+		number: "0"
+	}
+]
